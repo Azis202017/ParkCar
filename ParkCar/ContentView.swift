45240db2e3fe615bf24ebfc71802618d2e697149
Abdul Azis Al Ayubbi <azisa6980@gmail.com>
2024-07-06 03:34:24 +0700

    Refactor readme: added text

--- ParkCar/ContentView.swift
+++ ParkCar/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.primary).ignoresSafeArea()
+            
+            VStack (alignment: .leading){
+                HStack {
+                    Spacer()
+                    Image("logo").resizable().frame( width:238,height: 40)
+                    Spacer()
+                }.padding(.bottom, 80)
+                
+                Image("porsche").resizable().frame(height: 250)
+                VStack (alignment:.leading){
+                    Text("Get Your Secure Park").multilineTextAlignment(.leading).font(.system(size: 46)).foregroundColor(.white).fontWeight(.bold)
+                    Text("We provide thousands safe places for your beautiful and luxury cars").multilineTextAlignment(.leading).foregroundColor(Color("subtitle_text")).font(.system(size: 20)).padding(.top, 1.5)
+                    Button(action: getStarted, label: {
+                        HStack {
+                            Spacer()
+                            Text("Get Started").font(.system(size: 18).bold()).foregroundColor(.white)
+                            Spacer()
+                        }.padding(.all, 8)
+                    }).background(Color("button_color")).cornerRadius(50).padding(.top, 20)
+                }.padding(.horizontal,20)
+                
+            }
         }
-        .padding()
+    }
+    func getStarted() {
+        print("Get Started")
     }
 }
 
---
+++ Readme.md
@@ -0,0 +1,1 @@
+This repo just i want to learn swift ui base on learn by doing.
