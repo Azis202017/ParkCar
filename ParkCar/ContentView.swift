301aeda9d5d63fc08823c89da5dee4e111b0b831
Abdul Azis Al Ayubbi <azisa6980@gmail.com>
2024-07-06 02:42:29 +0700

    Initial Commit

---
+++ ParkCar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ParkCar
+//
+//  Created by Abdul Azis  Al Ayubbi on 06/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ParkCar/ParkCarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkCarApp.swift
+//  ParkCar
+//
+//  Created by Abdul Azis  Al Ayubbi on 06/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ParkCarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
